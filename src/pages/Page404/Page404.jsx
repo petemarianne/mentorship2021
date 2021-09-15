75594dd8db63d13a3d8ae0ae1fc2cf9fc1ec3bee
petemarianne <petia2001@icloud.com>
2021-09-15 22:32:47 +0300

    page404 and footer created

---
+++ src/pages/Page404/Page404.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Page404.scss';
+import logo from '../../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+
+const Page404 = () => {
+    return (
+        <>
+            <div className={'logo404-wrapper'}>
+                <div className={'four'}>4</div>
+                <div className={'logo'}><img src={logo} alt={'logo'}/></div>
+                <div className={'four'}>4</div>
+            </div>
+            <div className='message-wrapper'>
+                <div>Oops, this page was not found!</div>
+                <div>Either something went wrong or the page doesn't exist anymore.</div>
+            </div>
+            <div className='button-wrapper'>
+                <Button color='primary' variant='contained' component={Link} to='/' className={'go-home-button'}>Go Home</Button>
+            </div>
+        </>
+    )
+}
+
+export default Page404;
