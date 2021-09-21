15f077b0ba74e82b045ed2d07e65359a47dc3fa6
petemarianne <petia2001@icloud.com>
2021-09-21 11:36:08 +0300

    new lines added

--- src/store/actions/breedAction.js
+++ src/store/actions/breedAction.js
@@ -1,6 +1,6 @@
 import { BREED_SEARCH } from './actionTypes';
 
 export const breedSearch = (breed) => {
-    //document.body.setAttribute('theme', theme);
     return { type: BREED_SEARCH, breed };
 };
+
