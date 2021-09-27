608d26a4771742f2aad05fae1967189cd70c1e57
petemarianne <petia2001@icloud.com>
2021-09-27 17:22:31 +0300

    resolved conflicts

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import './Layout.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { Context } from '../../context';
+import {setCountryContext} from "../../pages/Main/Main";
 
 const Layout = ({ pageWrapperClass, children }) => {
   return (
-    <>
+    <Context.Provider value={{setCountryContext}}>
         <Header/>
         <main className={pageWrapperClass}>
             {children}
         </main>
         <Footer />
-    </>
+    </Context.Provider>
   );
 };
 
