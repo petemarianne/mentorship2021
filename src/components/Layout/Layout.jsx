114230f703dabe5e0f273ec481de8c7651c85b1e
petemarianne <petia2001@icloud.com>
2021-09-27 17:24:15 +0300

    delete unnecesairy files

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,18 +3,16 @@ import PropTypes from 'prop-types';
 import './Layout.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import { Context } from '../../context';
-import {setCountryContext} from "../../pages/Main/Main";
 
 const Layout = ({ pageWrapperClass, children }) => {
   return (
-    <Context.Provider value={{setCountryContext}}>
+    <>
         <Header/>
         <main className={pageWrapperClass}>
             {children}
         </main>
         <Footer />
-    </Context.Provider>
+    </>
   );
 };
 
