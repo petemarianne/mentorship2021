3f89d31d3b4f88890b73d4b78dba7ac525ef8349
petemarianne <petia2001@icloud.com>
2021-10-05 08:57:32 +0300

    create mobile and tablet details page

--- src/pages/Ad/Ad.jsx
+++ src/pages/Ad/Ad.jsx
@@ -4,8 +4,31 @@ import './Ad.scss';
 
 const Ad = () => {
     const screenSize = useMediaQuery('(min-width: 769px)');
+    const mobileScreenSize = useMediaQuery('(min-width: 426px)');
     const [ad] = useState(JSON.parse(localStorage.getItem('currentAd')))
     const [user] = useState(JSON.parse(localStorage.getItem('currentUser')))
+
+    const sellerInfoJSX =
+        <>
+            <div className='seller-info-wrapper'>
+                <div className='avatar-wrapper'><img src={user.avatar} alt='User avatar'/></div>
+                <div className='info-wrapper'>
+                    <div className='username'>{user.name}</div>
+                    <div className='ads-count'>Ads: {user.activeAds}</div>
+                    <div className='date'>On Dog Shop since {new Date(user.date.seconds * 1000).toLocaleString('default', {month: 'long',  year: 'numeric'})}</div>
+                </div>
+            </div>
+        </>;
+
+    const buttonsJSX =
+        <>
+            <div className='call-button-wrapper'>
+                <Button color='primary' variant='contained' className='call-button'>Call</Button>
+            </div>
+            <div className='email-button-wrapper'>
+                <Button color='primary' variant='outlined' className='email-button'>Email</Button>
+            </div>
+        </>;
 
     return (
         <>
@@ -20,25 +43,53 @@ const Ad = () => {
                             <div className='date'>published on {new Date(ad.date.seconds * 1000).toLocaleString('default', {day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric'})}</div>
                         </div>
                         <div className='buttons-seller-info-wrapper'>
-                            <div className='call-button-wrapper'>
-                                <Button color='primary' variant='contained' className='call-button'>Call</Button>
-                            </div>
-                            <div className='email-button-wrapper'>
-                                <Button color='primary' variant='outlined' className='email-button'>Email</Button>
-                            </div>
-                            <div className='seller-info-wrapper'>
-                                <div className='avatar-wrapper'><img src={user.avatar} alt='User avatar'/></div>
-                                <div className='info-wrapper'>
-                                    <div className='username'>{user.name}</div>
-                                    <div className='ads-count'>Ads: {user.activeAds}</div>
-                                    <div className='date'>On Dog Shop since {new Date(user.date.seconds * 1000).toLocaleString('default', {month: 'long',  year: 'numeric'})}</div>
-                                </div>
-                            </div>
+                            {buttonsJSX}
+                            {sellerInfoJSX}
                         </div>
                     </div>
                     <div className='description-wrapper'>
                         <div>Description</div>
                         <div className='description'>{ad.description}</div>
+                    </div>
+                </div>
+            )}
+            {!screenSize && mobileScreenSize && (
+                <div className='ad-page-tablet-wrapper'>
+                    <div className='pic-wrapper-wrapper'><div className='pic-wrapper'><img src={ad.picture} alt={'Ad picture'}/></div></div>
+                    <div className='info-wrapper'>
+                        <div className='date'>{new Date(ad.date.seconds * 1000).toLocaleString('default', {day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric'})}</div>
+                        <div className='breed-location-price-buttons-wrapper'>
+                            <div className='breed-location-price-wrapper'>
+                                <div className='breed'>{ad.title}</div>
+                                <div className='location'>{ad.city}, {ad.country}</div>
+                                <div className='price'>{ad.price}$</div>
+                            </div>
+                            <div className='buttons-wrapper'>
+                                {buttonsJSX}
+                            </div>
+                        </div>
+                        <div className='description-title'>Description</div>
+                        <div className='description'>{ad.description}</div>
+                        {sellerInfoJSX}
+                    </div>
+                </div>
+            )}
+            {!screenSize && !mobileScreenSize && (
+                <div className='ad-page-mobile-wrapper'>
+                    <div className='pic-wrapper-wrapper'><div className='pic-wrapper'><img src={ad.picture} alt={'Ad picture'}/></div></div>
+                    <div className='info-wrapper'>
+                        <div className='date'>{new Date(ad.date.seconds * 1000).toLocaleString('default', {day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric'})}</div>
+                        <div className='breed-location-price-wrapper'>
+                            <div className='breed-location-wrapper'>
+                                <div className='breed'>{ad.title}</div>
+                                <div className='location'>{ad.city}, {ad.country}</div>
+                            </div>
+                            <div className='price'>{ad.price}$</div>
+                        </div>
+                        <div className='description-title'>Description</div>
+                        <div className='description'>{ad.description}</div>
+                        {buttonsJSX}
+                        {sellerInfoJSX}
                     </div>
                 </div>
             )}
