5500a9f4bfff3cd6901caea7fe73e63a934f0b3a
petemarianne <petia2001@icloud.com>
2021-10-08 11:31:19 +0300

    delete unused file

--- src/utils/generateID.js
+++
@@ -1,9 +0,0 @@
-export const generateID = () => {
-    let id = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
-    for (let i = 0; i < 30; i++) {
-        id += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return id;
-}
