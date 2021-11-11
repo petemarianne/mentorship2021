0f578884be29927b09016a27907e625e34db02a4
Piatrovich Piotr <ppiatrovich@exadel.net>
2021-11-11 20:14:46 +0300

    create register end point

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,51 @@
 import { Router } from 'express';
+import {check, validationResult} from 'express-validator';
+import { users } from '../data/users.js';
+import bcrypt from 'bcryptjs';
 
 const authRouter = Router();
 
-authRouter.post('/register', async (req, res) => {
+authRouter.post(
+    '/register',
+    [
+       check('email', 'Invalid email').isEmail(),
+        check('password', 'Password must be more than 6 symbols').isLength({min: 6})
+    ],
+    async (req, res) => {
     try {
-        const { email, password, name, phone, pic } = req.body;
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Invalid data!'
+            });
+        }
+
+        const { email, password, name, phone, avatar } = req.body;
+
+        const userIndex = users.findIndex(item => item.email === email);
+        if (userIndex >= 0) {
+            return res.status(400).json({message: 'This email is already used!'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = {
+            email,
+            password: hashedPassword,
+            name,
+            phone,
+            avatar,
+            date: {
+                seconds: new Date().getTime() / 1000,
+                nanoseconds: 0,
+            },
+            activeAds: 0,
+            id: `seller${users.length + 1}`
+        };
+
+        users.push(user);
+
     } catch (e) {
         return res.status(500);
     }
