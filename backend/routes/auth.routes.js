f5d39b53233877eb4bc5e14d480cb578ca3645eb
Piatrovich Piotr <ppiatrovich@exadel.net>
2021-11-12 10:53:25 +0300

    create login api

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import {check, validationResult} from 'express-validator';
 import { users } from '../data/users.js';
 import bcrypt from 'bcryptjs';
+import jsonwebtoken from 'jsonwebtoken';
+import config from 'config';
 
 const authRouter = Router();
 
@@ -46,13 +48,50 @@ authRouter.post(
 
         users.push(user);
 
+        return res.status(201).json({...user});
     } catch (e) {
         return res.status(500);
     }
 });
 
-authRouter.post('/login', async (req, res) => {
+authRouter.post('/login',
+    [
+        check('email', 'Invalid email').normalizeEmail().isEmail(),
+        check('password', 'Password must be more than 6 symbols').exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
 
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Invalid data!'
+                });
+            }
+
+            const {email, password} = req.body;
+
+            const userIndex = users.findIndex(item => item.email === email);
+            if (userIndex >= 0) {
+                return res.status(404).json({message: 'User is not found!'});
+            }
+
+            const isMatch = await bcrypt.compare(password, users[userIndex].password);
+            if (!isMatch) {
+                return res.status(400).json({message: 'Incorrect password!'});
+            }
+
+            const token = jsonwebtoken.sign(
+                {userID: users[userIndex].id},
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            );
+
+            return res.status(200).json({token, userID: users[userIndex].id});
+        } catch (e) {
+            return res.status(500);
+        }
 });
 
 export default authRouter;
