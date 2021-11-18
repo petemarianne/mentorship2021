db483459d01c39b22e189bb56b48d3586b42baab
Piatrovich Piotr <ppiatrovich@exadel.net>
2021-11-19 02:42:56 +0300

    app.js to app.ts

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import config from 'config';
-import adsRouter from './backend/routes/ads.routes.js';
-import usersRouter from './backend/routes/users.routes.js';
-import authRouter from './backend/routes/auth.routes.js';
+import adsRouter from './backend/routes/ads.routes';
+import usersRouter from './backend/routes/users.routes';
+import authRouter from './backend/routes/auth.routes';
 
 const app = express();
 
+// @ts-ignore
 app.use(express.json({ extended: true }));
 
 const API = '/api';
@@ -20,7 +21,7 @@ const start = () => {
     try {
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
     } catch (e) {
-        console.log('Server Error ', e.message());
+        console.log('Server Error');
         process.exit(1);
     }
 };
