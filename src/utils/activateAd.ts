dd7659e01cdd204f073471f50ddfa251fdd522de
Piatrovich Piotr <ppiatrovich@exadel.net>
2021-11-19 14:20:22 +0300

    remove generic before localStorage usage

--- src/utils/activateAd.ts
+++ src/utils/activateAd.ts
@@ -1,6 +1,6 @@
 import { Ad } from '../interfaces';
 
 export const activateAd = async (ad: Ad): Promise<void> => {
-    fetch(`/api/ads/${ad.id}`, {method: 'PUT', body: JSON.stringify({status: 'active'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(<string>localStorage.getItem('userData')).token}});
-    fetch(`/api/users/${JSON.parse(<string>localStorage.getItem('userData')).userId}`, {method: 'PUT', body: JSON.stringify({action: 'add'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(<string>localStorage.getItem('userData')).token}});
+    fetch(`/api/ads/${ad.id}`, {method: 'PUT', body: JSON.stringify({status: 'active'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(localStorage.getItem('userData') as string).token}});
+    fetch(`/api/users/${JSON.parse(localStorage.getItem('userData') as string).userId}`, {method: 'PUT', body: JSON.stringify({action: 'add'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(localStorage.getItem('userData') as string).token}});
 };
--- src/utils/sellAd.ts
+++ src/utils/sellAd.ts
@@ -1,6 +1,6 @@
 import { Ad} from '../interfaces';
 
 export const sellAd = async (ad: Ad): Promise<void> => {
-    fetch(`/api/ads/${ad.id}`, {method: 'PUT', body: JSON.stringify({status: 'sold'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(<string>localStorage.getItem('userData')).token}});
-    fetch(`/api/users/${JSON.parse(<string>localStorage.getItem('userData')).userId}`, {method: 'PUT', body: JSON.stringify({action: 'remove'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(<string>localStorage.getItem('userData')).token}});
+    fetch(`/api/ads/${ad.id}`, {method: 'PUT', body: JSON.stringify({status: 'sold'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(localStorage.getItem('userData') as string).token}});
+    fetch(`/api/users/${JSON.parse(localStorage.getItem('userData') as string).userId}`, {method: 'PUT', body: JSON.stringify({action: 'remove'}), headers: {'Content-Type': 'application/json', 'authorization': JSON.parse(localStorage.getItem('userData') as string).token}});
 };
