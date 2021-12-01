f70d050c94a9d7315caf8827df1e581a148c06d6
Pete Marianne <petemarianne@Petes-MacBook-Pro.local>
2021-12-01 17:04:40 +0300

    refactor: remove comments

--- backend/utils/comparatorAds.ts
+++ backend/utils/comparatorAds.ts
@@ -2,22 +2,18 @@ import { Ad } from '../../src/interfaces';
 
 export const comparator = (item1: Ad, item2: Ad, sort: any) => {
     if (item1.price === item2.price) {
-        // @ts-ignore
-        return item2.date - item1.date;
+        return item2.date.valueOf() - item1.date.valueOf();
     }
     switch (sort) {
         case 'dateDown':
-            // @ts-ignore
-            return item2.date - item1.date;
+            return item2.date.valueOf() - item1.date.valueOf();
         case 'dateUp':
-            // @ts-ignore
-            return item1.date - item2.date;
+            return item1.date.valueOf() - item2.date.valueOf();
         case 'priceDown':
             return item2.price - item1.price;
         case 'priceUp':
             return item1.price - item2.price;
         default:
-            // @ts-ignore
-            return item2.date - item1.date;
+            return item2.date.valueOf() - item1.date.valueOf();
     }
 };
